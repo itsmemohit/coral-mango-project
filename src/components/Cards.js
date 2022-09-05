de6cab33c6b95f5973c401de38430f04ff3aef4f
itsmemohit <mohitdangwal016.md@gmail.com>
2022-09-06 01:15:21 +0530

    Developed cards with search feature and dummy data

---
+++ src/components/Cards.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import Card from 'react-bootstrap/Card'
+
+const Cards = ({ data }) => {
+    return (
+        <>
+            {
+                data.map((element, k) => {
+                    return (
+                        <>
+                            <Card style={{ width: '22rem', border: "none" }} className="hove mb-4">
+                                <Card.Img variant="top" className='cd' src={element.imgdata} />
+
+                                <div className="card_body">
+                                    <div className="upper_data d-flex justify-content-between align-items-center">
+                                        <h4 className='mt-2'>{element.rname}</h4>
+                                        <span>{element.rating}&nbsp;★</span>
+                                    </div>
+
+                                    <div className="lower_data d-flex  justify-content-between">
+                                        <h5>{element.address}</h5>
+                                        <span>{element.price}</span>
+                                    </div>
+                                </div>
+
+                            </Card>
+                        </>
+                    )
+                })
+            }
+
+        </>
+    )
+}
+
+export default Cards
---
+++ src/components/FoodData.js
@@ -0,0 +1,68 @@
+const Fooddata = [
+    {
+        id: 1,
+        rname: "Massala Theoryy",
+        imgdata: "https://b.zmtcdn.com/data/pictures/9/18857339/8f53919f1175c08cf0f0371b73704f9b_o2_featured_v2.jpg?output-format=webp",
+        address: "North Indian, Biryani, Mughlai",
+        rating: "3.8",
+
+    },
+    {
+        id: 2,
+        rname: "Jugaadi Adda",
+        imgdata: "https://b.zmtcdn.com/data/pictures/chains/5/19295245/089cbcf1d3307542c72f77272556b28b_o2_featured_v2.jpg?output-format=webp",
+        address: "Street Food",
+        rating: "3.9",
+    },
+    {
+        id: 3,
+        rname: "La Milano Pizzeria",
+        imgdata: "https://b.zmtcdn.com/data/pictures/chains/1/19708611/10f90d4a69678d98662514d173b29665_o2_featured_v2.jpg",
+        address: "Pizza, Fast Food, Pasta",
+        rating: "4.2",
+    },
+    {
+        id: 4,
+        rname: "Momoman",
+        imgdata: "https://b.zmtcdn.com/data/pictures/chains/1/113401/59f29399060caefcc575d59dc9402ce8_o2_featured_v2.jpg",
+        address: "Momos",
+        rating: "3.8",
+    },
+    {
+        id: 5,
+        rname: "Jassi De Parathe",
+        imgdata: "https://b.zmtcdn.com/data/pictures/chains/5/110225/3978e28854b7496dbef9496546734811_o2_featured_v2.jpg",
+        address: "North Indian",
+        rating: "4.0",
+    },
+    {
+        id: 6,
+        rname: "Anjoy Latenight Meals",
+        imgdata: "https://b.zmtcdn.com/data/pictures/5/113895/3c06f6fbb8f667a2537dfdb6f060dc8b_o2_featured_v2.jpg",
+        address: "Wraps FastFood, Chines",
+        rating: "3.8",
+    },
+    {
+        id: 7,
+        rname: "Hocco Eatery",
+        imgdata: "https://b.zmtcdn.com/data/pictures/chains/5/110155/811c01a5430d50d3260f77917da99e12_o2_featured_v2.jpg",
+        address: "North Indian, Fast Food",
+        rating: "3.8",
+    },
+    {
+        id: 8,
+        rname: "Chai Wai",
+        imgdata: "https://b.zmtcdn.com/data/pictures/3/18514413/0a17b72e9fec52a3ca736f4c2ea3646f_o2_featured_v2.jpg",
+        address: "Tea, Coffee, Shake, Beverages",
+        rating: "3.2",
+    },
+    {
+        id: 9,
+        rname: "HL Frankie",
+        imgdata: "https://b.zmtcdn.com/data/pictures/7/19639627/94c0a4cf15c02d3982d154e2c5dd8cbb_o2_featured_v2.jpg",
+        address: "Burger, Sandwich, Fast Food",
+        rating: "3.8",
+    },
+];
+
+export default Fooddata;
